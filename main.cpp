99581f6194c7b01dd2fc5b9778ac535dc3324fad
VedMed-Niratug <z24i@yandex.ru>
2024-06-15 13:52:40 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template <typename Documents>
+ostream& operator<< (ostream& out, const Documents& documents) {
+    bool first = true;
+    for (const auto& document : documents) {
+        if (!first) {
+            out << ", "s << document;
+        }
+        out << document;
+        first = false;
+    }
+    return out;
+}
+
+int main() {
+    const vector<int> ages = { 10, 5, 2, 12 };
+    cout << ages << endl;
+    const set<string> cats = { "Мурка"s, "Белка"s, "Георгий"s, "Рюрик"s };
+    cout << cats << endl;
+    return 0;
+}
